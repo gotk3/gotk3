a5c41c04efd3230e770b8ac6deb7e7277230017b
Josh Rickmar <jrick@conformal.com>
2013-07-13 15:48:21 -0400

    Documentation for gdk.

--- gdk/gdk.go
+++ gdk/gdk.go
@@ -83,8 +83,10 @@ const (
  * GdkAtom
  */
 
+// Atom is a representation of GDK's GdkAtom.
 type Atom uintptr
 
+// Native() returns the underlying GdkAtom.
 func (v Atom) Native() C.GdkAtom {
 	return C.toGdkAtom(unsafe.Pointer(v))
 }
@@ -93,10 +95,12 @@ func (v Atom) Native() C.GdkAtom {
  * GdkDevice
  */
 
+// Device is a representation of GDK's GdkDevice.
 type Device struct {
 	*glib.Object
 }
 
+// Native() returns a pointer to the underlying GdkDevice
 func (v *Device) Native() *C.GdkDevice {
 	if v == nil || v.GObject == nil {
 		return nil
@@ -109,10 +113,12 @@ func (v *Device) Native() *C.GdkDevice {
  * GdkDeviceManager
  */
 
+// DeviceManager is a representation of GDK's GdkDeviceManager.
 type DeviceManager struct {
 	*glib.Object
 }
 
+// Native() returns a pointer to the underlying GdkDeviceManager.
 func (v *DeviceManager) Native() *C.GdkDeviceManager {
 	if v == nil || v.GObject == nil {
 		return nil
@@ -125,10 +131,12 @@ func (v *DeviceManager) Native() *C.GdkDeviceManager {
  * GdkDisplay
  */
 
+// Display is a representation of GDK's GdkDisplay.
 type Display struct {
 	*glib.Object
 }
 
+// Native() returns a pointer to the underlying GdkDisplay.
 func (v *Display) Native() *C.GdkDisplay {
 	if v == nil || v.GObject == nil {
 		return nil
@@ -137,6 +145,7 @@ func (v *Display) Native() *C.GdkDisplay {
 	return C.toGdkDisplay(p)
 }
 
+// DisplayOpen() is a wrapper around gdk_display_open().
 func DisplayOpen(displayName string) (*Display, error) {
 	cstr := C.CString(displayName)
 	defer C.free(unsafe.Pointer(cstr))
@@ -151,6 +160,7 @@ func DisplayOpen(displayName string) (*Display, error) {
 	return d, nil
 }
 
+// DisplayGetDefault() is a wrapper around gdk_display_get_default().
 func DisplayGetDefault() (*Display, error) {
 	c := C.gdk_display_get_default()
 	if c == nil {
@@ -163,6 +173,7 @@ func DisplayGetDefault() (*Display, error) {
 	return d, nil
 }
 
+// GetName() is a wrapper around gdk_display_get_name().
 func (v *Display) GetName() (string, error) {
 	c := C.gdk_display_get_name(v.Native())
 	if c == nil {
@@ -171,11 +182,13 @@ func (v *Display) GetName() (string, error) {
 	return C.GoString((*C.char)(c)), nil
 }
 
+// GetNScreens() is a wrapper around gdk_display_get_n_screens().
 func (v *Display) GetNScreens() int {
 	c := C.gdk_display_get_n_screens(v.Native())
 	return int(c)
 }
 
+// GetScreen() is a wrapper around gdk_display_get_screen().
 func (v *Display) GetScreen(screenNum int) (*Screen, error) {
 	c := C.gdk_display_get_screen(v.Native(), C.gint(screenNum))
 	if c == nil {
@@ -188,6 +201,7 @@ func (v *Display) GetScreen(screenNum int) (*Screen, error) {
 	return s, nil
 }
 
+// GetDefaultScreen() is a wrapper around gdk_display_get_default_screen().
 func (v *Display) GetDefaultScreen() (*Screen, error) {
 	c := C.gdk_display_get_default_screen(v.Native())
 	if c == nil {
@@ -200,6 +214,7 @@ func (v *Display) GetDefaultScreen() (*Screen, error) {
 	return s, nil
 }
 
+// GetDeviceManager() is a wrapper around gdk_display_get_device_manager().
 func (v *Display) GetDeviceManager() (*DeviceManager, error) {
 	c := C.gdk_display_get_device_manager(v.Native())
 	if c == nil {
@@ -212,32 +227,39 @@ func (v *Display) GetDeviceManager() (*DeviceManager, error) {
 	return d, nil
 }
 
+// DeviceIsGrabbed() is a wrapper around gdk_display_device_is_grabbed().
 func (v *Display) DeviceIsGrabbed(device *Device) bool {
 	c := C.gdk_display_device_is_grabbed(v.Native(), device.Native())
 	return gobool(c)
 }
 
+// Beep() is a wrapper around gdk_display_beep().
 func (v *Display) Beep() {
 	C.gdk_display_beep(v.Native())
 }
 
+// Sync() is a wrapper around gdk_display_sync().
 func (v *Display) Sync() {
 	C.gdk_display_sync(v.Native())
 }
 
+// Flush() is a wrapper around gdk_display_flush().
 func (v *Display) Flush() {
 	C.gdk_display_flush(v.Native())
 }
 
+// Close() is a wrapper around gdk_display_close().
 func (v *Display) Close() {
 	C.gdk_display_close(v.Native())
 }
 
+// IsClosed() is a wrapper around gdk_display_is_closed().
 func (v *Display) IsClosed() bool {
 	c := C.gdk_display_is_closed(v.Native())
 	return gobool(c)
 }
 
+// GetEvent() is a wrapper around gdk_display_get_event().
 func (v *Display) GetEvent() (*Event, error) {
 	c := C.gdk_display_get_event(v.Native())
 	if c == nil {
@@ -248,6 +270,7 @@ func (v *Display) GetEvent() (*Event, error) {
 	return e, nil
 }
 
+// PeekEvent() is a wrapper around gdk_display_peek_event().
 func (v *Display) PeekEvent() (*Event, error) {
 	c := C.gdk_display_peek_event(v.Native())
 	if c == nil {
@@ -258,44 +281,53 @@ func (v *Display) PeekEvent() (*Event, error) {
 	return e, nil
 }
 
+// PutEvent() is a wrapper around gdk_display_put_event().
 func (v *Display) PutEvent(event *Event) {
 	C.gdk_display_put_event(v.Native(), event.Native())
 }
 
+// HasPending() is a wrapper around gdk_display_has_pending().
 func (v *Display) HasPending() bool {
 	c := C.gdk_display_has_pending(v.Native())
 	return gobool(c)
 }
 
+// SetDoubleClickTime() is a wrapper around gdk_display_set_double_click_time().
 func (v *Display) SetDoubleClickTime(msec uint) {
 	C.gdk_display_set_double_click_time(v.Native(), C.guint(msec))
 }
 
+// SetDoubleClickDistance() is a wrapper around gdk_display_set_double_click_distance().
 func (v *Display) SetDoubleClickDistance(distance uint) {
 	C.gdk_display_set_double_click_distance(v.Native(), C.guint(distance))
 }
 
+// SupportsColorCursor() is a wrapper around gdk_display_supports_cursor_color().
 func (v *Display) SupportsColorCursor() bool {
 	c := C.gdk_display_supports_cursor_color(v.Native())
 	return gobool(c)
 }
 
+// SupportsCursorAlpha() is a wrapper around gdk_display_supports_cursor_alpha().
 func (v *Display) SupportsCursorAlpha() bool {
 	c := C.gdk_display_supports_cursor_alpha(v.Native())
 	return gobool(c)
 }
 
+// GetDefaultCursorSize() is a wrapper around gdk_display_get_default_cursor_size().
 func (v *Display) GetDefaultCursorSize() uint {
 	c := C.gdk_display_get_default_cursor_size(v.Native())
 	return uint(c)
 }
 
+// GetMaximalCursorSize() is a wrapper around gdk_display_get_maximal_cursor_size().
 func (v *Display) GetMaximalCursorSize() (width, height uint) {
 	var w, h C.guint
 	C.gdk_display_get_maximal_cursor_size(v.Native(), &w, &h)
 	return uint(w), uint(h)
 }
 
+// GetDefaultGroup() is a wrapper around gdk_display_get_default_group().
 func (v *Display) GetDefaultGroup() (*Window, error) {
 	c := C.gdk_display_get_default_group(v.Native())
 	if c == nil {
@@ -308,17 +340,23 @@ func (v *Display) GetDefaultGroup() (*Window, error) {
 	return w, nil
 }
 
+// SupportsSelectionNotification() is a wrapper around
+// gdk_display_supports_selection_notification().
 func (v *Display) SupportsSelectionNotification() bool {
 	c := C.gdk_display_supports_selection_notification(v.Native())
 	return gobool(c)
 }
 
+// RequestSelectionNotification() is a wrapper around
+// gdk_display_request_selection_notification().
 func (v *Display) RequestSelectionNotification(selection Atom) bool {
 	c := C.gdk_display_request_selection_notification(v.Native(),
 		selection.Native())
 	return gobool(c)
 }
 
+// SupportsClipboardPersistence() is a wrapper around
+// gdk_display_supports_clipboard_persistence().
 func (v *Display) SupportsClipboardPersistence() bool {
 	c := C.gdk_display_supports_clipboard_persistence(v.Native())
 	return gobool(c)
@@ -328,16 +366,19 @@ func (v *Display) SupportsClipboardPersistence() bool {
 func (v *Display) StoreClipboard(clipboardWindow *Window, time uint32, targets ...Atom) {
 }
 
+// SupportsShapes() is a wrapper around gdk_display_supports_shapes().
 func (v *Display) SupportsShapes() bool {
 	c := C.gdk_display_supports_shapes(v.Native())
 	return gobool(c)
 }
 
+// SupportsInputShapes() is a wrapper around gdk_display_supports_input_shapes().
 func (v *Display) SupportsInputShapes() bool {
 	c := C.gdk_display_supports_input_shapes(v.Native())
 	return gobool(c)
 }
 
+// SupportsComposite() is a wrapper around gdk_display_supports_composite().
 func (v *Display) SupportsComposite() bool {
 	c := C.gdk_display_supports_composite(v.Native())
 	return gobool(c)
@@ -347,6 +388,7 @@ func (v *Display) SupportsComposite() bool {
 func (v *Display) GetAppLaunchContext() {
 }
 
+// NotifyStartupComplete() is a wrapper around gdk_display_notify_startup_complete().
 func (v *Display) NotifyStartupComplete(startupID string) {
 	cstr := C.CString(startupID)
 	defer C.free(unsafe.Pointer(cstr))
@@ -357,10 +399,12 @@ func (v *Display) NotifyStartupComplete(startupID string) {
  * GdkEvent
  */
 
+// Event is a representation of GDK's GdkEvent.
 type Event struct {
 	GdkEvent *C.GdkEvent
 }
 
+// Native() returns a pointer to the underlying GdkEvent.
 func (v *Event) Native() *C.GdkEvent {
 	if v == nil {
 		return nil
@@ -376,10 +420,12 @@ func (v *Event) free() {
  * GdkScreen
  */
 
+// Screen is a representation of GDK's GdkScreen.
 type Screen struct {
 	*glib.Object
 }
 
+// Native() returns a pointer to the underlying GdkScreen.
 func (v *Screen) Native() *C.GdkScreen {
 	if v == nil || v.GObject == nil {
 		return nil
@@ -392,10 +438,12 @@ func (v *Screen) Native() *C.GdkScreen {
  * GdkWindow
  */
 
+// Window is a representation of GDK's GdkWindow.
 type Window struct {
 	*glib.Object
 }
 
+// Native() returns a pointer to the underlying GdkWindow.
 func (v *Window) Native() *C.GdkWindow {
 	if v == nil || v.GObject == nil {
 		return nil
