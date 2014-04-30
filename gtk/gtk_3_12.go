5ad8b3e9bef80b7567609ecaaea767c1ce69f9a3
Josh Rickmar <jrick@conformal.com>
2014-04-29 22:16:52 -0500

    Add initial Popover bindings (for GTK >= 3.12).
    The bindings depending on unimplemented GMenuModel and GdkRectangle
    bindings have been marked as TODO for now.

--- gtk/gtk_3_12.go
+++ gtk/gtk_3_12.go
@@ -26,7 +26,108 @@ package gtk
 // #cgo pkg-config: gtk+-3.0
 // #include <stdlib.h>
 // #include <gtk/gtk.h>
+// #include "gtk_3_12.go.h"
 import "C"
+import (
+	"github.com/conformal/gotk3/glib"
+	"runtime"
+	"unsafe"
+)
+
+func init() {
+	tm := []glib.TypeMarshaler{
+		// Objects/Interfaces
+		{glib.Type(C.gtk_popover_get_type()), marshalPopover},
+	}
+	glib.RegisterGValueMarshalers(tm)
+}
+
+/*
+ * GtkPopover
+ */
+
+// Popover is a representation of GTK's GtkPopover.
+type Popover struct {
+	Bin
+}
+
+// native returns a pointer to the underlying GtkPopover.
+func (v *Popover) native() *C.GtkPopover {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkPopover(p)
+}
+
+func marshalPopover(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(c))}
+	return wrapPopover(obj), nil
+}
+
+func wrapPopover(obj *glib.Object) *Popover {
+	return &Popover{Bin{Container{Widget{glib.InitiallyUnowned{obj}}}}}
+}
+
+// PopoverNew is a wrapper around gtk_popover_new().
+func PopoverNew(relativeTo IWidget) (*Popover, error) {
+	c := C.gtk_popover_new(relativeTo.toWidget())
+	if c == nil {
+		return nil, nilPtrErr
+	}
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(c))}
+	a := wrapPopover(obj)
+	obj.RefSink()
+	runtime.SetFinalizer(obj, (*glib.Object).Unref)
+	return a, nil
+}
+
+// TODO: gtk_popover_new_from_model
+// TODO: gtk_popover_bind_model
+
+// SetRelativeTo is a wrapper around gtk_popover_set_relative_to().
+func (v *Popover) SetRelativeTo(relativeTo IWidget) {
+	C.gtk_popover_set_relative_to(v.native(), relativeTo.toWidget())
+}
+
+// GetRelativeTo is a wrapper around gtk_popover_get_relative_to().
+func (v *Popover) GetRelativeTo() (*Widget, error) {
+	c := C.gtk_popover_get_relative_to(v.native())
+	if c == nil {
+		return nil, nilPtrErr
+	}
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(c))}
+	w := wrapWidget(obj)
+	obj.RefSink()
+	runtime.SetFinalizer(obj, (*glib.Object).Unref)
+	return w, nil
+}
+
+// TODO: gtk_popover_set_pointing_to
+// TODO: gtk_popover_get_pointing_to
+
+// SetPosition is a wrapper around gtk_popover_set_position().
+func (v *Popover) SetPosition(position PositionType) {
+	C.gtk_popover_set_position(v.native(), C.GtkPositionType(position))
+}
+
+// GetPosition is a wrapper around gtk_popover_get_position().
+func (v *Popover) GetPosition() PositionType {
+	c := C.gtk_popover_get_position(v.native())
+	return PositionType(c)
+}
+
+// SetModal is a wrapper around gtk_popover_set_modal().
+func (v *Popover) SetModal(modal bool) {
+	C.gtk_popover_set_modal(v.native(), gbool(modal))
+}
+
+// GetModal is a wrapper around gtk_popover_get_modal().
+func (v *Popover) GetModal() bool {
+	c := C.gtk_popover_get_modal(v.native())
+	return gobool(c)
+}
 
 /*
  * GtkWidget
