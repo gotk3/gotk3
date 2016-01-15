8190acd3b0864b40685ef53fd7a9dc253d863532
Oleg Yamnikov <bgeyts667@gmail.com>
2016-01-15 17:13:13 +0500

    Implement GList elements data wrapping.

--- glib/list.go
+++ glib/list.go
@@ -14,6 +14,10 @@ import "unsafe"
 // List is a representation of Glib's GList.
 type List struct {
 	list *C.struct__GList
+	// If set, dataWrap is called every time NthDataWrapped()
+	// or DataWrapped() is called to wrap raw underlying
+	// value into appropriate type.
+	dataWrap func(unsafe.Pointer) interface{}
 }
 
 func WrapList(obj uintptr) *List {
@@ -21,7 +25,14 @@ func WrapList(obj uintptr) *List {
 }
 
 func wrapList(obj *C.struct__GList) *List {
-	return &List{obj}
+	return &List{list: obj}
+}
+
+func (v *List) wrapNewHead(obj *C.struct__GList) *List {
+	return &List{
+		list:     obj,
+		dataWrap: v.dataWrap,
+	}
 }
 
 func (v *List) Native() uintptr {
@@ -35,22 +46,29 @@ func (v *List) native() *C.struct__GList {
 	return v.list
 }
 
+// DataWapper sets wrap functions, which is called during NthDataWrapped()
+// and DataWrapped(). It's used to cast raw C data into appropriate
+// Go structures and types every time that data is retreived.
+func (v *List) DataWrapper(fn func(unsafe.Pointer) interface{}) {
+	v.dataWrap = fn
+}
+
 // Append is a wrapper around g_list_append().
 func (v *List) Append(data uintptr) *List {
 	glist := C.g_list_append(v.native(), C.gpointer(data))
-	return wrapList(glist)
+	return v.wrapNewHead(glist)
 }
 
 // Prepend is a wrapper around g_list_prepend().
 func (v *List) Prepend(data uintptr) *List {
 	glist := C.g_list_prepend(v.native(), C.gpointer(data))
-	return wrapList(glist)
+	return v.wrapNewHead(glist)
 }
 
 // Insert is a wrapper around g_list_insert().
 func (v *List) Insert(data uintptr, position int) *List {
 	glist := C.g_list_insert(v.native(), C.gpointer(data), C.gint(position))
-	return wrapList(glist)
+	return v.wrapNewHead(glist)
 }
 
 // Length is a wrapper around g_list_length().
@@ -59,8 +77,19 @@ func (v *List) Length() uint {
 }
 
 // NthData is a wrapper around g_list_nth_data().
-func (v *List) NthData(n uint) interface{} {
-	return C.g_list_nth_data(v.native(), C.guint(n))
+func (v *List) NthData(n uint) unsafe.Pointer {
+	return unsafe.Pointer(C.g_list_nth_data(v.native(), C.guint(n)))
+}
+
+// NthDataWrapped acts the same as NthData(), but passes
+// retrieved value before returning through wrap function, set by DataWrapper().
+// If no wrap function is set, it returns raw unsafe.Pointer.
+func (v *List) NthDataWrapped(n uint) interface{} {
+	ptr := v.NthData(n)
+	if v.dataWrap != nil {
+		return v.dataWrap(ptr)
+	}
+	return ptr
 }
 
 // Free is a wrapper around g_list_free().
@@ -70,15 +99,26 @@ func (v *List) Free() {
 
 // Next is a wrapper around the next struct field
 func (v *List) Next() *List {
-	return wrapList(v.native().next)
+	return v.wrapNewHead(v.native().next)
 }
 
 // Previous is a wrapper around the prev struct field
 func (v *List) Previous() *List {
-	return wrapList(v.native().prev)
+	return v.wrapNewHead(v.native().prev)
 }
 
 // Data is a wrapper around the data struct field
 func (v *List) Data() unsafe.Pointer {
 	return unsafe.Pointer(v.native().data)
+}
+
+// DataWrapped acts the same as Data(), but passes
+// retrieved value before returning through wrap function, set by DataWrapper().
+// If no wrap function is set, it returns raw unsafe.Pointer.
+func (v *List) DataWrapped() interface{} {
+	ptr := v.Data()
+	if v.dataWrap != nil {
+		return v.dataWrap(ptr)
+	}
+	return ptr
 }
