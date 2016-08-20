363ba5726d7e9b3a8a5ea4a2b7a2674ebceaba0f
flan <flan@flande.re>
2016-08-20 17:41:18 +0200

    Add ScrolledWindow.GetOverlayScrolling() and ScrolledWindow.SetOverlayScrolling()

---
+++ gtk/gtk_since_3_16.go
@@ -0,0 +1,19 @@
+// +build !gtk_3_6,!gtk_3_8,!gtk_3_10,!gtk_3_12,!gtk_3_14
+
+// See: https://developer.gnome.org/gtk3/3.16/api-index-3-16.html
+
+package gtk
+
+// #cgo pkg-config: gtk+-3.0
+// #include <gtk/gtk.h>
+import "C"
+
+// SetOverlayScrolling is a wrapper around gtk_scrolled_window_set_overlay_scrolling().
+func (v *ScrolledWindow) SetOverlayScrolling(scrolling bool) {
+    C.gtk_scrolled_window_set_overlay_scrolling(v.native(), gbool(scrolling))
+}
+
+// GetOverlayScrolling is a wrapper around gtk_scrolled_window_get_overlay_scrolling().
+func (v *ScrolledWindow) GetOverlayScrolling() bool {
+    return gobool(C.gtk_scrolled_window_get_overlay_scrolling(v.native()))
+}
