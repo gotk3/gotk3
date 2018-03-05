f670c8a58f4d4abe03036f00b3ce817a7d32335d
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2018-03-05 18:49:17 +0100

    Implement functions for GtkStatusIcon as far as possible

--- gtk/statusicon.go
+++ gtk/statusicon.go
@@ -16,6 +16,16 @@ import "github.com/gotk3/gotk3/gdk"
 // TODO: GtkStatusIcon * gtk_status_icon_new_from_gicon (GIcon *icon);
 // TODO: void gtk_status_icon_set_from_gicon (GtkStatusIcon *status_icon, GIcon *icon);
 
+// TODO: GIcon * gtk_status_icon_get_gicon (GtkStatusIcon *status_icon);
+
+// TODO: void gtk_status_icon_set_screen (GtkStatusIcon *status_icon, GdkScreen *screen);
+// TODO: GdkScreen * gtk_status_icon_get_screen (GtkStatusIcon *status_icon);
+
+// TODO: GdkPixbuf * gtk_status_icon_get_pixbuf (GtkStatusIcon *status_icon);
+
+// TODO: void gtk_status_icon_position_menu (GtkMenu *menu, gint *x, gint *y, gboolean *push_in, gpointer user_data);
+// TODO: gboolean gtk_status_icon_get_geometry (GtkStatusIcon *status_icon, GdkScreen **screen, GdkRectangle *area, GtkOrientation *orientation);
+
 // StatusIcon is a representation of GTK's GtkStatusIcon.
 // Deprecated since 3.14 in favor of notifications
 // (no replacement, see https://stackoverflow.com/questions/41917903/gtk-3-statusicon-replacement)
@@ -99,38 +109,129 @@ func StatusIconNewFromPixbuf(pixbuf *gdk.Pixbuf) (*StatusIcon, error) {
 	return wrapStatusIcon(obj), nil
 }
 
-// TODO:
-// void gtk_status_icon_set_from_pixbuf (GtkStatusIcon *status_icon, GdkPixbuf *pixbuf);
-// void gtk_status_icon_set_from_file (GtkStatusIcon *status_icon, const gchar *filename);
-// void gtk_status_icon_set_from_stock (GtkStatusIcon *status_icon, const gchar *stock_id);
-//    Deprecated since 3.10 Use gtk_status_icon_set_from_icon_name() instead.
-// void gtk_status_icon_set_from_icon_name (GtkStatusIcon *status_icon, const gchar *icon_name);
+// SetFromPixbuf is a wrapper around gtk_status_icon_set_from_pixbuf()
+func (v *StatusIcon) SetFromPixbuf(pixbuf *gdk.Pixbuf) {
+	C.gtk_status_icon_set_from_pixbuf(v.native(), C.toGdkPixbuf(unsafe.Pointer(pixbuf.Native())))
+}
 
-// GtkImageType gtk_status_icon_get_storage_type (GtkStatusIcon *status_icon);
+// SetFromFile is a wrapper around gtk_status_icon_set_from_file()
+func (v *StatusIcon) SetFromFile(filename string) {
+	cstr := C.CString(filename)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_from_file(v.native(), (*C.gchar)(cstr))
+}
 
-// GdkPixbuf * gtk_status_icon_get_pixbuf (GtkStatusIcon *status_icon);
-// const gchar * gtk_status_icon_get_stock (GtkStatusIcon *status_icon);
-// const gchar * gtk_status_icon_get_icon_name (GtkStatusIcon *status_icon);
-// GIcon * gtk_status_icon_get_gicon (GtkStatusIcon *status_icon);
+// SetFromStock is a wrapper around gtk_status_icon_set_from_stock()
+// Deprecated since 3.10, use SetFromIconName (gtk_status_icon_set_from_icon_name) instead.
+func (v *StatusIcon) SetFromStock(stockID string) {
+	cstr := C.CString(stockID)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_from_stock(v.native(), (*C.gchar)(cstr))
+}
 
-// gint gtk_status_icon_get_size (GtkStatusIcon *status_icon);
+// SetFromIconName is a wrapper around gtk_status_icon_set_from_icon_name()
+func (v *StatusIcon) SetFromIconName(iconName string) {
+	cstr := C.CString(iconName)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_from_icon_name(v.native(), (*C.gchar)(cstr))
+}
 
-// void gtk_status_icon_set_screen (GtkStatusIcon *status_icon, GdkScreen *screen);
-// GdkScreen * gtk_status_icon_get_screen (GtkStatusIcon *status_icon);
+// GetStorageType is a wrapper around gtk_status_icon_get_storage_type
+func (v *StatusIcon) GetStorageType() ImageType {
+	c := C.gtk_status_icon_get_storage_type(v.native())
+	return ImageType(c)
+}
 
-// void gtk_status_icon_set_tooltip_text (GtkStatusIcon *status_icon, const gchar *text);
-// gchar * gtk_status_icon_get_tooltip_text (GtkStatusIcon *status_icon);
+// GetStock is a wrapper around gtk_status_icon_get_stock()
+// Deprecated since 3.10, use GetIconName (gtk_status_icon_get_icon_name) instead
+func (v *StatusIcon) GetStock() string {
+	c := C.gtk_status_icon_get_stock(v.native())
+	if c == nil {
+		return ""
+	}
+	return C.GoString((*C.char)(c))
+}
 
-// void gtk_status_icon_set_tooltip_markup (GtkStatusIcon *status_icon, const gchar *markup);
-// gchar * gtk_status_icon_get_tooltip_markup (GtkStatusIcon *status_icon);
+// GetIconName is a wrapper around gtk_status_icon_get_icon_name()
+func (v *StatusIcon) GetIconName() string {
+	c := C.gtk_status_icon_get_icon_name(v.native())
+	if c == nil {
+		return ""
+	}
+	return C.GoString((*C.char)(c))
+}
 
-// void gtk_status_icon_set_has_tooltip (GtkStatusIcon *status_icon, gboolean has_tooltip);
-// gboolean gtk_status_icon_get_has_tooltip (GtkStatusIcon *status_icon);
+// GetSize is a wrapper around gtk_status_icon_get_size()
+func (v *StatusIcon) GetSize() int {
+	c := C.gtk_status_icon_get_size(v.native())
+	return int(c)
+}
 
-// void gtk_status_icon_set_title (GtkStatusIcon *status_icon, const gchar *title);
-// const gchar * gtk_status_icon_get_title (GtkStatusIcon *status_icon);
+// SetTooltipText is a wrapper around gtk_status_icon_set_tooltip_text()
+func (v *StatusIcon) SetTooltipText(text string) {
+	cstr := C.CString(text)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_tooltip_text(v.native(), (*C.gchar)(cstr))
+}
 
-// void gtk_status_icon_set_name (GtkStatusIcon *status_icon, const gchar *name);
+// GetTooltipText is a wrapper around gtk_status_icon_get_tooltip_text()
+func (v *StatusIcon) GetTooltipText() string {
+	c := C.gtk_status_icon_get_tooltip_text(v.native())
+	if c == nil {
+		return ""
+	}
+	return C.GoString((*C.char)(c))
+}
+
+// SetTooltipMarkup is a wrapper around gtk_status_icon_set_tooltip_markup()
+func (v *StatusIcon) SetTooltipMarkup(markup string) {
+	cstr := C.CString(markup)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_tooltip_markup(v.native(), (*C.gchar)(cstr))
+}
+
+// GetTooltipMarkup is a wrapper around gtk_status_icon_get_tooltip_markup()
+func (v *StatusIcon) GetTooltipMarkup() string {
+	c := C.gtk_status_icon_get_tooltip_markup(v.native())
+	if c == nil {
+		return ""
+	}
+	return C.GoString((*C.char)(c))
+}
+
+// SetHasTooltip is a wrapper around gtk_status_icon_set_has_tooltip()
+func (v *StatusIcon) SetHasTooltip(hasTooltip bool) {
+	C.gtk_status_icon_set_has_tooltip(v.native(), gbool(hasTooltip))
+}
+
+// GetHasTooltip is a wrapper around gtk_status_icon_get_has_tooltip()
+func (v *StatusIcon) GetHasTooltip() bool {
+	c := C.gtk_status_icon_get_has_tooltip(v.native())
+	return gobool(c)
+}
+
+// SetTitle is a wrapper around gtk_status_icon_set_title()
+func (v *StatusIcon) SetTitle(title string) {
+	cstr := C.CString(title)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_title(v.native(), (*C.gchar)(cstr))
+}
+
+// GetTitle is a wrapper around gtk_status_icon_get_title()
+func (v *StatusIcon) GetTitle() string {
+	c := C.gtk_status_icon_get_title(v.native())
+	if c == nil {
+		return ""
+	}
+	return C.GoString((*C.char)(c))
+}
+
+// SetName is a wrapper around gtk_status_icon_set_name()
+func (v *StatusIcon) SetName(name string) {
+	cstr := C.CString(name)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_status_icon_set_name(v.native(), (*C.gchar)(cstr))
+}
 
 // SetVisible is a wrapper around gtk_status_icon_set_visible()
 func (v *StatusIcon) SetVisible(visible bool) {
@@ -143,8 +244,13 @@ func (v *StatusIcon) GetVisible() bool {
 	return gobool(c)
 }
 
-// gboolean gtk_status_icon_is_embedded (GtkStatusIcon *status_icon);
+// IsEmbedded is a wrapper around gtk_status_icon_is_embedded()
+func (v *StatusIcon) IsEmbedded() bool {
+	c := C.gtk_status_icon_is_embedded(v.native())
+	return gobool(c)
+}
 
-// void gtk_status_icon_position_menu (GtkMenu *menu, gint *x, gint *y, gboolean *push_in, gpointer user_data);
-// gboolean gtk_status_icon_get_geometry (GtkStatusIcon *status_icon, GdkScreen **screen, GdkRectangle *area, GtkOrientation *orientation);
-// guint32 gtk_status_icon_get_x11_window_id (GtkStatusIcon *status_icon);
+// GetX11WindowID is a wrapper around gtk_status_icon_get_x11_window_id
+func (v *StatusIcon) GetX11WindowID() uint32 {
+	return uint32(C.gtk_status_icon_get_x11_window_id(v.native()))
+}
