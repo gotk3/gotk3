94070d3be1c1add5934aadb85afa23bbc42aebcd
Andreas Raster <lazor@affenbande.org>
2018-04-02 01:04:48 +0200

    implement gtk_tree_view_column_get/set_order

--- gtk/tree_view_column.go
+++ gtk/tree_view_column.go
@@ -274,8 +274,24 @@ func (v *TreeViewColumn) GetSizing() TreeViewColumnSizing {
 // GtkWidget * 	gtk_tree_view_column_get_button ()
 // void 	gtk_tree_view_column_set_alignment ()
 // gfloat 	gtk_tree_view_column_get_alignment ()
+
+type GtkSortType int
+
+const (
+	GTK_SORT_ASCENDING  GtkSortType = C.GTK_SORT_ASCENDING
+	GTK_SORT_DESCENDING             = C.GTK_SORT_DESCENDING
+)
+
 // void 	gtk_tree_view_column_set_sort_order ()
+func (v *TreeViewColumn) SetSortOrder(order GtkSortType) {
+	C.gtk_tree_view_column_set_sort_order(v.native(), C.GtkSortType(order))
+}
+
 // GtkSortType 	gtk_tree_view_column_get_sort_order ()
+func (v *TreeViewColumn) GetSortOrder() GtkSortType {
+	return GtkSortType(C.gtk_tree_view_column_get_sort_order(v.native()))
+}
+
 // void 	gtk_tree_view_column_cell_set_cell_data ()
 // void 	gtk_tree_view_column_cell_get_size ()
 // gboolean 	gtk_tree_view_column_cell_get_position ()
