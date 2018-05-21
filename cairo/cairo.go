f33574abe3c82836cf36919b72d136c4bc3acacc
yatsouba <ivan.yatsouba@gmail.com>
2018-05-21 12:36:24 +0300

    static compile libcairo

--- cairo/cairo.go
+++ cairo/cairo.go
@@ -19,7 +19,7 @@
 package cairo
 
 // #cgo pkg-config: cairo cairo-gobject
-// #cgo LDFLAGS: -L./lib
+// #cgo LDFLAGS: -L./lib -static
 // #include <stdlib.h>
 // #include <cairo.h>
 // #include <cairo-gobject.h>
