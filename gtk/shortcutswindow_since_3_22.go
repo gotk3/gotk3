01d49d99364cac665eaeb64a6a6931bb87d163f9
Marcel Schramm <marceloschr@googlemail.com>
2018-11-14 16:05:39 +0100

    Manually merge additions of GtkShortcutsGroup/Section/Shortcut by xsmurf

--- gtk/shortcutswindow_since_3_22.go
+++ gtk/shortcutswindow_since_3_22.go
@@ -15,11 +15,17 @@ import (
 func init() {
 	tm := []glib.TypeMarshaler{
 		{glib.Type(C.gtk_shortcuts_window_get_type()), marshalShortcutsWindow},
+		{glib.Type(C.gtk_shortcuts_section_get_type()), marshalShortcutsSection},
+		{glib.Type(C.gtk_shortcuts_group_get_type()), marshalShortcutsGroup},
+		{glib.Type(C.gtk_shortcuts_shortcut_get_type()), marshalShortcutsShortcut},
 	}
 
 	glib.RegisterGValueMarshalers(tm)
 
 	WrapMap["GtkShortcutsWindow"] = wrapShortcutsWindow
+	WrapMap["GtkShortcutsSection"] = wrapShortcutsSection
+	WrapMap["GtkShortcutsGroup"] = wrapShortcutsGroup
+	WrapMap["GtkShortcutsShortcut"] = wrapShortcutsShortcut
 }
 
 /*
@@ -47,4 +53,89 @@ func marshalShortcutsWindow(p uintptr) (interface{}, error) {
 
 func wrapShortcutsWindow(obj *glib.Object) *ShortcutsWindow {
 	return &ShortcutsWindow{Window{Bin{Container{Widget{glib.InitiallyUnowned{obj}}}}}}
+}
+
+/*
+ * GtkShortcutsSection
+ */
+
+// ShortcutsWindow is a representation of GTK's GtkShortcutsSection.
+type ShortcutsSection struct {
+	Box
+}
+
+// native returns a pointer to the underlying GtkShortcutsSection.
+func (v *ShortcutsSection) native() *C.GtkShortcutsSection {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkShortcutsSection(p)
+}
+
+func marshalShortcutsSection(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapShortcutsSection(obj), nil
+}
+
+func wrapShortcutsSection(obj *glib.Object) *ShortcutsSection {
+	return &ShortcutsSection{Box{Container{Widget{glib.InitiallyUnowned{obj}}}}}
+}
+
+/*
+ * GtkShortcutsSection
+ */
+
+// ShortcutsWindow is a representation of GTK's GtkShortcutsGroup.
+type ShortcutsGroup struct {
+	Box
+}
+
+// native returns a pointer to the underlying GtkShortcutsGroup.
+func (v *ShortcutsGroup) native() *C.GtkShortcutsGroup {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkShortcutsGroup(p)
+}
+
+func marshalShortcutsGroup(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapShortcutsGroup(obj), nil
+}
+
+func wrapShortcutsGroup(obj *glib.Object) *ShortcutsGroup {
+	return &ShortcutsGroup{Box{Container{Widget{glib.InitiallyUnowned{obj}}}}}
+}
+
+
+/*
+ * GtkShortcutsShortcut
+ */
+
+// ShortcutsWindow is a representation of GTK's GtkShortcutsShortcut.
+type ShortcutsShortcut struct {
+	Box
+}
+
+// native returns a pointer to the underlying GtkShortcutsShortcut.
+func (v *ShortcutsShortcut) native() *C.GtkShortcutsShortcut {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkShortcutsShortcut(p)
+}
+
+func marshalShortcutsShortcut(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapShortcutsShortcut(obj), nil
+}
+
+func wrapShortcutsShortcut(obj *glib.Object) *ShortcutsShortcut {
+	return &ShortcutsShortcut{Box{Container{Widget{glib.InitiallyUnowned{obj}}}}}
 }
