3da9fc6514a21edf9ead5b047134511f057b63b0
patknight <patknight.github@gmail.com>
2019-03-07 09:43:34 -0500

    Update glib_test.go

--- glib/glib_test.go
+++ glib/glib_test.go
@@ -4,8 +4,8 @@ import (
 	"runtime"
 	"testing"
 
-	"github.com/patknight/gotk3/glib"
-	"github.com/patknight/gotk3/gtk"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 )
 
 func init() {
