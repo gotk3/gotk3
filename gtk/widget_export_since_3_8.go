9f6308f08f822d2ae3a10ece06a2f08b6f1f8718
drakbar <drakbar96@gmail.com>
2019-03-14 20:07:27 -0500

    Added Go functions to handle TickCall conversions

---
+++ gtk/widget_export_since_3_8.go
@@ -0,0 +1,28 @@
+// +build !gtk_3_6
+
+package gtk
+
+// #include <gdk/gdk.h>
+// #include <gtk/gtk.h>
+import "C"
+import (
+	"unsafe"
+
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
+)
+
+//export goTickCallbacks
+func goTickCallbacks (widget *C.GtkWidget, frameClock *C.GdkFrameClock, userData C.gpointer) C.gboolean {
+	id := int(uintptr(userData))
+
+	tickCallbackRegistry.Lock()
+	r := tickCallbackRegistry.m[id]
+	tickCallbackRegistry.Unlock()
+
+	return gbool(r.fn(
+		wrapWidget(glib.Take(unsafe.Pointer(widget))), 
+		gdk.WrapFrameClock(unsafe.Pointer(frameClock)),
+		r.userData,
+	))
+}
