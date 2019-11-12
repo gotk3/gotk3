5976f983ab208135360f80c812b8bde9720c310b
LegBone <cowby123@gmail.com>
2019-11-12 20:47:13 +0800

    fix_cicd error
    fix_cicd error

--- gdk/window_x11.go
+++ gdk/window_x11.go
@@ -39,7 +39,7 @@ func (v *Window) GetXID() uint32 {
 //ForeignNewForDisplay is a wrapper around gdk_x11_window_foreign_new_for_display()
 // It only works on GDK versions compiled with X11 support - its return value can't be used if WorkspaceControlSupported returns false
 func (v *Display) ForeignNewForDisplay(xid uint32) (*Window, error) {
-	c := C.gdk_x11_window_foreign_new_for_display(v.native(), C.guint64(xid))
+	c := C.gdk_x11_window_foreign_new_for_display(v.native(), C.Window(xid))
 	if c == nil {
 		return nil, nilPtrErr
 	}
