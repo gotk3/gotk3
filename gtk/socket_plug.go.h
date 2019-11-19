94958460dd7dba7c1c507373f47061c3f6bb2fdb
graynk <gray.nk@gmail.com>
2019-11-19 17:22:55 +0500

    support for GdkSocket and GdkPlug

---
+++ gtk/socket_plug.go.h
@@ -0,0 +1,18 @@
+
+static GtkSocket *
+toGtkSocket(void *p)
+{
+	return (GTK_SOCKET(p));
+}
+
+static GtkPlug *
+toGtkPlug(void *p)
+{
+	return (GTK_PLUG(p));
+}
+
+static GdkDisplay *
+toGdkDisplay(void *p)
+{
+	return (GDK_DISPLAY(p));
+}
