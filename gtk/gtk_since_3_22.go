7cf2074b1e05fb1cc872af2f4566db8c645059e2
MJacred <loesch.benny92@gmx.de>
2020-01-09 14:56:41 +0100

    add bindings for gtk_popover_popup and gtk_popover_popdown

---
+++ gtk/gtk_since_3_22.go
@@ -0,0 +1,18 @@
+// +build !gtk_3_6,!gtk_3_8,!gtk_3_10,!gtk_3_12,!gtk_3_14,!gtk_3_16,!gtk_3_18,!gtk_3_20
+
+// See: https://developer.gnome.org/gtk3/3.22/api-index-3-22.html
+
+package gtk
+
+// #include <gtk/gtk.h>
+import "C"
+
+// Popup is a wrapper around gtk_popover_popup().
+func (v *Popover) Popup() {
+	C.gtk_popover_popup(v.native())
+}
+
+// Popdown is a wrapper around gtk_popover_popdown().
+func (v *Popover) Popdown() {
+	C.gtk_popover_popdown(v.native())
+}
