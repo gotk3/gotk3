d2125b61c2ce258df93efeef26962fa4ec5e423f
MJacred <loesch.benny92@gmx.de>
2020-01-30 16:22:27 +0100

    export Permission's Native(), add WrapPermission()

--- glib/gpermission.go
+++ glib/gpermission.go
@@ -12,8 +12,8 @@ type Permission struct {
 	*Object
 }
 
-// native returns a pointer to the underlying GPermission.
-func (v *Permission) native() *C.GPermission {
+// Native returns a pointer to the underlying GPermission.
+func (v *Permission) Native() *C.GPermission {
 	if v == nil || v.GObject == nil {
 		return nil
 	}
@@ -22,31 +22,35 @@ func (v *Permission) native() *C.GPermission {
 
 func marshalPermission(p uintptr) (interface{}, error) {
 	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
-	return &Permission{wrapObject(unsafe.Pointer(c))}, nil
+	return WrapPermission(c), nil
+}
+
+func WrapPermission(c *C.GPermission) *Permission {
+	return &Permission{wrapObject(unsafe.Pointer(c))}
 }
 
 // GetAllowed is a wrapper around g_permission_get_allowed().
 func (v *Permission) GetAllowed() bool {
-	c := C.g_permission_get_allowed(v.native())
+	c := C.g_permission_get_allowed(v.Native())
 	return gobool(c)
 }
 
 // GetCanAcquire is a wrapper around g_permission_get_can_acquire().
 func (v *Permission) GetCanAcquire() bool {
-	c := C.g_permission_get_can_acquire(v.native())
+	c := C.g_permission_get_can_acquire(v.Native())
 	return gobool(c)
 }
 
 // GetCanRelease is a wrapper around g_permission_get_can_release().
 func (v *Permission) GetCanRelease() bool {
-	c := C.g_permission_get_can_release(v.native())
+	c := C.g_permission_get_can_release(v.Native())
 	return gobool(c)
 }
 
 // Acquire is a wrapper around g_permission_acquire().
 // func (v *Permission) Acquire(cancellable *Cancellable) error {
 // 	var err *C.GError
-// 	c := C.g_permission_acquire(v.native(), cancellable.native(), &err)
+// 	c := C.g_permission_acquire(v.Native(), cancellable.Native(), &err)
 // 	acquired := gobool(c)
 // 	if !acquired {
 // 		defer C.g_error_free(err)
@@ -57,13 +61,13 @@ func (v *Permission) GetCanRelease() bool {
 
 // AcquireAsync is a wrapper around g_permission_acquire_async().
 // func (v *Permission) AcquireAsync(cancellable *Cancellable, callback AsyncReadyCallback, data uintptr) {
-// 	C.g_permission_acquire_async(v.native(), cancellable.native(), , C.gpointer(data))
+// 	C.g_permission_acquire_async(v.Native(), cancellable.Native(), , C.gpointer(data))
 // }
 
 // AcquireFinish is a wrapper around g_permission_acquire_finish().
 // func (v *Permission) AcquireFinish(result *AsyncResult) error {
 // 	var err *C.GError
-// 	c := C.g_permission_acquire_finish(v.native(), , &err)
+// 	c := C.g_permission_acquire_finish(v.Native(), , &err)
 // 	acquired := gobool(c)
 // 	if !acquired {
 // 		defer C.g_error_free(err)
@@ -75,7 +79,7 @@ func (v *Permission) GetCanRelease() bool {
 // Release is a wrapper around g_permission_release().
 // func (v *Permission) Release(cancellable *Cancellable) error {
 // 	var err *C.GError
-// 	c := C.g_permission_release(v.native(), cancellable.native(), &err)
+// 	c := C.g_permission_release(v.Native(), cancellable.Native(), &err)
 // 	released := gobool(c)
 // 	if !released {
 // 		defer C.g_error_free(err)
@@ -86,13 +90,13 @@ func (v *Permission) GetCanRelease() bool {
 
 // ReleaseAsync is a wrapper around g_permission_release_async().
 // func (v *Permission) ReleaseAsync(cancellable *Cancellable, callback AsyncReadyCallback, data uintptr) {
-// 	C.g_permission_release_async(v.native(), cancellable.native(), , C.gpointer(data))
+// 	C.g_permission_release_async(v.Native(), cancellable.Native(), , C.gpointer(data))
 // }
 
 // ReleaseFinish is a wrapper around g_permission_release_finish().
 // func (v *Permission) ReleaseFinish(result *AsyncResult) error {
 // 	var err *C.GError
-// 	c := C.g_permission_release_finish(v.native(), , &err)
+// 	c := C.g_permission_release_finish(v.Native(), , &err)
 // 	released := gobool(c)
 // 	if !released {
 // 		defer C.g_error_free(err)
@@ -103,5 +107,5 @@ func (v *Permission) GetCanRelease() bool {
 
 // ImplUpdate is a wrapper around g_permission_impl_update().
 func (v *Permission) ImplUpdate(allowed, canAcquire, canRelease bool) {
-	C.g_permission_impl_update(v.native(), gbool(allowed), gbool(canAcquire), gbool(canRelease))
+	C.g_permission_impl_update(v.Native(), gbool(allowed), gbool(canAcquire), gbool(canRelease))
 }
