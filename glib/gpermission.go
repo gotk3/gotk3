9ed349617ec3e5da7bf7f9db1a1ec88e8ca20ea3
MJacred <loesch.benny92@gmx.de>
2020-02-12 13:58:03 +0100

    fix wrong wrapping of glib.Permission

--- glib/gpermission.go
+++ glib/gpermission.go
@@ -16,12 +16,16 @@ type Permission struct {
 	*Object
 }
 
-// Native returns a pointer to the underlying GPermission.
-func (v *Permission) Native() *C.GPermission {
+func (v *Permission) native() *C.GPermission {
 	if v == nil || v.GObject == nil {
 		return nil
 	}
 	return C.toGPermission(unsafe.Pointer(v.GObject))
+}
+
+// Native returns a uintptr to the underlying C.GPermission.
+func (v *Permission) Native() uintptr {
+	return uintptr(unsafe.Pointer(v.native()))
 }
 
 func marshalPermission(p uintptr) (interface{}, error) {
@@ -33,32 +37,33 @@ func wrapPermission(obj *Object) *Permission {
 	return &Permission{obj}
 }
 
+// WrapPermission wraps given unsafe pointer into Permission.
 func WrapPermission(ptr unsafe.Pointer) *Permission {
-	return &Permission{wrapObject(ptr)}
+	return wrapPermission(wrapObject(ptr))
 }
 
 // GetAllowed is a wrapper around g_permission_get_allowed().
 func (v *Permission) GetAllowed() bool {
-	c := C.g_permission_get_allowed(v.Native())
+	c := C.g_permission_get_allowed(v.native())
 	return gobool(c)
 }
 
 // GetCanAcquire is a wrapper around g_permission_get_can_acquire().
 func (v *Permission) GetCanAcquire() bool {
-	c := C.g_permission_get_can_acquire(v.Native())
+	c := C.g_permission_get_can_acquire(v.native())
 	return gobool(c)
 }
 
 // GetCanRelease is a wrapper around g_permission_get_can_release().
 func (v *Permission) GetCanRelease() bool {
-	c := C.g_permission_get_can_release(v.Native())
+	c := C.g_permission_get_can_release(v.native())
 	return gobool(c)
 }
 
 // Acquire is a wrapper around g_permission_acquire().
 func (v *Permission) Acquire(cancellable *Cancellable) error {
 	var err *C.GError
-	c := C.g_permission_acquire(v.Native(), cancellable.native(), &err)
+	c := C.g_permission_acquire(v.native(), cancellable.native(), &err)
 	acquired := gobool(c)
 	if !acquired {
 		defer C.g_error_free(err)
@@ -71,14 +76,14 @@ func (v *Permission) Acquire(cancellable *Cancellable) error {
 func (v *Permission) AcquireAsync(cancellable *Cancellable, callback AsyncReadyCallback, userData uintptr) {
 
 	id := registerAsyncReadyCallback(callback, userData)
-	
+
 	C._g_permission_acquire_async(v.native(), cancellable.native(), C.gpointer(uintptr(id)))
 }
 
 // AcquireFinish is a wrapper around g_permission_acquire_finish().
 func (v *Permission) AcquireFinish(result *AsyncResult) error {
 	var err *C.GError
-	c := C.g_permission_acquire_finish(v.Native(), result.native(), &err)
+	c := C.g_permission_acquire_finish(v.native(), result.native(), &err)
 	acquired := gobool(c)
 	if !acquired {
 		defer C.g_error_free(err)
@@ -90,7 +95,7 @@ func (v *Permission) AcquireFinish(result *AsyncResult) error {
 // Release is a wrapper around g_permission_release().
 func (v *Permission) Release(cancellable *Cancellable) error {
 	var err *C.GError
-	c := C.g_permission_release(v.Native(), cancellable.native(), &err)
+	c := C.g_permission_release(v.native(), cancellable.native(), &err)
 	released := gobool(c)
 	if !released {
 		defer C.g_error_free(err)
@@ -110,7 +115,7 @@ func (v *Permission) ReleaseAsync(cancellable *Cancellable, callback AsyncReadyC
 // ReleaseFinish is a wrapper around g_permission_release_finish().
 func (v *Permission) ReleaseFinish(result *AsyncResult) error {
 	var err *C.GError
-	c := C.g_permission_release_finish(v.Native(), result.native(), &err)
+	c := C.g_permission_release_finish(v.native(), result.native(), &err)
 	released := gobool(c)
 	if !released {
 		defer C.g_error_free(err)
@@ -121,5 +126,5 @@ func (v *Permission) ReleaseFinish(result *AsyncResult) error {
 
 // ImplUpdate is a wrapper around g_permission_impl_update().
 func (v *Permission) ImplUpdate(allowed, canAcquire, canRelease bool) {
-	C.g_permission_impl_update(v.Native(), gbool(allowed), gbool(canAcquire), gbool(canRelease))
+	C.g_permission_impl_update(v.native(), gbool(allowed), gbool(canAcquire), gbool(canRelease))
 }
