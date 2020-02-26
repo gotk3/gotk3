99e59ea2febd36c476f20d4e329d0bbd88600947
Andreas Bergmeier <abergmeier@gmx.net>
2020-02-26 15:46:00 +0100

    Implement Device.GetPosition, Display.GetDefaultSeat, Seat.GetPointer.

--- gdk/gdk_since_3_20.go
+++ gdk/gdk_since_3_20.go
@@ -3,7 +3,14 @@
 
 package gdk
 
+import (
+	"unsafe"
+
+	"github.com/gotk3/gotk3/glib"
+)
+
 // #include <gdk/gdk.h>
+// #include "gdk_since_3_20.go.h"
 import "C"
 
 /*
@@ -19,9 +26,11 @@ func (v *GLContext) IsLegacy() bool {
  * GdkDisplay
  */
 
-// gdk_display_get_default_seat().
-// gdk_display_list_seats().
+func (v *Display) GetDefaultSeat() (*Seat, error) {
+	return toSeat(C.gdk_display_get_default_seat(v.native()))
+}
 
+// gdk_display_list_seats().
 
 /*
  * GdkDevice
@@ -30,3 +39,42 @@ func (v *GLContext) IsLegacy() bool {
 // TODO:
 // gdk_device_get_axes().
 // gdk_device_get_seat().
+
+/*
+ * GdkSeat
+ */
+
+type Seat struct {
+	*glib.Object
+}
+
+func (v *Seat) native() *C.GdkSeat {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGdkSeat(p)
+}
+
+// Native returns a pointer to the underlying GdkCursor.
+func (v *Seat) Native() uintptr {
+	return uintptr(unsafe.Pointer(v.native()))
+}
+
+func marshalSeat(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(c))}
+	return &Seat{obj}, nil
+}
+
+func toSeat(s *C.GdkSeat) (*Seat, error) {
+	if s == nil {
+		return nil, nilPtrErr
+	}
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(s))}
+	return &Seat{obj}, nil
+}
+
+func (v *Seat) GetPointer() (*Device, error) {
+	return toDevice(C.gdk_seat_get_pointer(v.native()))
+}
