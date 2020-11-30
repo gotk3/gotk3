7bac31bb1d458ed27e83b5e25743a1f9772d2852
diamondburned <datutbrus@gmail.com>
2020-11-30 07:56:33 -0800

    Add back CAIRO_ANTIALIAS_FAST and co.

--- cairo/antialias.go
+++ cairo/antialias.go
@@ -16,9 +16,9 @@ const (
 	ANTIALIAS_NONE     Antialias = C.CAIRO_ANTIALIAS_NONE
 	ANTIALIAS_GRAY     Antialias = C.CAIRO_ANTIALIAS_GRAY
 	ANTIALIAS_SUBPIXEL Antialias = C.CAIRO_ANTIALIAS_SUBPIXEL
-	// ANTIALIAS_FAST     Antialias = C.CAIRO_ANTIALIAS_FAST (since 1.12)
-	// ANTIALIAS_GOOD     Antialias = C.CAIRO_ANTIALIAS_GOOD (since 1.12)
-	// ANTIALIAS_BEST     Antialias = C.CAIRO_ANTIALIAS_BEST (since 1.12)
+	ANTIALIAS_FAST     Antialias = C.CAIRO_ANTIALIAS_FAST // (since 1.12)
+	ANTIALIAS_GOOD     Antialias = C.CAIRO_ANTIALIAS_GOOD // (since 1.12)
+	ANTIALIAS_BEST     Antialias = C.CAIRO_ANTIALIAS_BEST // (since 1.12)
 )
 
 func marshalAntialias(p uintptr) (interface{}, error) {
