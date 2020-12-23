bebf551cc1837f540a66612db822a278348347df
H.F.M <45952208+hfmrow@users.noreply.github.com>
2020-12-23 10:15:00 +0100

    Add binding for TextView & TextAnchor
    File modified: 'text_child_anchor.go'
    gtk_text_child_anchor_new
    gtk_text_child_anchor_get_widgets
    gtk_text_child_anchor_get_deleted
    
    File modified: 'gtk_since_3_18.go'
    gtk_text_view_set_top_margin
    gtk_text_view_get_top_margin
    gtk_text_view_set_bottom_margin
    gtk_text_view_get_bottom_margin
    
    File modified: 'text_view.go'
    Removing TODO informations relative to added methods above.

--- gtk/text_child_anchor.go
+++ gtk/text_child_anchor.go
@@ -5,6 +5,11 @@ package gtk
 // #include <gtk/gtk.h>
 // #include "gtk.go.h"
 import "C"
+import (
+	"unsafe"
+
+	"github.com/gotk3/gotk3/glib"
+)
 
 /*
  * GtkTextChildAnchor
@@ -16,4 +21,30 @@ type TextChildAnchor C.GtkTextChildAnchor
 // native returns a pointer to the underlying GtkTextChildAnchor.
 func (v *TextChildAnchor) native() *C.GtkTextChildAnchor {
 	return (*C.GtkTextChildAnchor)(v)
+}
+
+// TextChildAnchorNew is a wrapper around gtk_text_child_anchor_new ()
+func TextChildAnchorNew() *TextChildAnchor {
+	ret := C.gtk_text_child_anchor_new()
+	return (*TextChildAnchor)(ret)
+}
+
+// GetWidgets is a wrapper around gtk_text_child_anchor_get_widgets ().
+func (v *TextChildAnchor) GetWidgets() *glib.List {
+	clist := C.gtk_text_child_anchor_get_widgets(v.native())
+	if clist == nil {
+		return nil
+	}
+
+	glist := glib.WrapList(uintptr(unsafe.Pointer(clist)))
+	glist.DataWrapper(func(ptr unsafe.Pointer) interface{} {
+		return wrapWidget(glib.Take(ptr))
+	})
+
+	return glist
+}
+
+// GetDeleted is a wrapper around gtk_text_child_anchor_get_deleted().
+func (v *TextChildAnchor) GetDeleted() bool {
+	return gobool(C.gtk_text_child_anchor_get_deleted(v.native()))
 }
