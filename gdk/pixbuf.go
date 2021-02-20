c9075ee736c308e9b77abce272ff5f93382e853d
James Wright <james@jamesswright.co.uk>
2021-02-20 15:13:18 +0000

    Adds bindings for gdk_pixbuf_new_from_resource() and gdk_pixbuf_new_from_resource_at_scale()

--- gdk/pixbuf.go
+++ gdk/pixbuf.go
@@ -177,6 +177,50 @@ func PixbufNewFromDataOnly(pixbufData []byte) (*Pixbuf, error) {
 	return pixbufLoader.WriteAndReturnPixbuf(pixbufData)
 }
 
+// PixbufNewFromResource is a wrapper around gdk_pixbuf_new_from_resource()
+func PixbufNewFromResource(resourcePath string) (*gdk.Pixbuf, error) {
+	var gerr *C.GError
+
+	cstr := C.CString(resourcePath)
+	defer C.free(unsafe.Pointer(cstr))
+
+	c := C.gdk_pixbuf_new_from_resource((*C.gchar)(cstr), &gerr)
+
+	if gerr != nil {
+		defer C.g_error_free(gerr)
+		return nil, errors.New(C.GoString(gerr.message))
+	}
+
+	obj := glib.Take(unsafe.Pointer(c))
+	return &gdk.Pixbuf{obj}, nil
+}
+
+// PixbufNewFromResourceAtScale is a wrapper around gdk_pixbuf_new_from_resource_at_scale()
+func PixbufNewFromResourceAtScale(resourcePath string, width, height int, preserveAspectRatio bool) (*gdk.Pixbuf, error) {
+	var gerr *C.GError
+
+	cstr := C.CString(resourcePath)
+	defer C.free(unsafe.Pointer(cstr))
+
+	cPreserveAspectRatio := gbool(presereAspectRatio)
+
+	c := C.gdk_pixbuf_new_from_resource_at_scale(
+		(*C.gchar)(cstr),
+		C.gint(width),
+		C.gint(height),
+		C.gboolean(cPreserveAspectRatio),
+		&gerr,
+	)
+
+	if gerr != nil {
+		defer C.g_error_free(gerr)
+		return nil, errors.New(C.GoString(gerr.message))
+	}
+
+	obj := glib.Take(unsafe.Pointer(c))
+	return &gdk.Pixbuf{obj}, nil
+}
+
 // TODO:
 // gdk_pixbuf_new_from_xpm_data().
 // gdk_pixbuf_new_subpixbuf()
