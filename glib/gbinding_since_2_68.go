dcd7d0b4c61e204121f316665e7bc05efb1196b7
Ola Bini <ola@olabini.se>
2022-09-15 18:48:01 +0000

    Update Binding build tags to only build on later versions of Glib. This adds the build tags in both the old and new format, for maximum compatibility.

--- glib/gbinding_since_2_68.go
+++ glib/gbinding_since_2_68.go
@@ -1,4 +1,5 @@
-// +build !glib_deprecated
+//go:build !glib_deprecated && !glib_2_40 && !glib_2_42 && !glib_2_44 && !glib_2_46 && !glib_2_48 && !glib_2_50 && !glib_2_52 && !glib_2_54 && !glib_2_56 && !glib_2_58 && !glib_2_60 && !glib_2_62 && !glib_2_64 && !glib_2_66
+// +build !glib_deprecated,!glib_2_40,!glib_2_42,!glib_2_44,!glib_2_46,!glib_2_48,!glib_2_50,!glib_2_52,!glib_2_54,!glib_2_56,!glib_2_58,!glib_2_60,!glib_2_62,!glib_2_64,!glib_2_66
 
 package glib
 
